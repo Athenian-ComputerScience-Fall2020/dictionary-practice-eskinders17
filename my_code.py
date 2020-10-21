37cab74ba8638afeeda6804c84c35ede8d16de5a
eskinders17 <22ehaileyesus@athenian.org>
2020-10-21 09:11:23 +0000

    FINISHED

--- my_code.py
+++ my_code.py
@@ -7,7 +7,7 @@ def make_dict():
     keys = ['Ten', 'Twenty', 'Thirty']
     values = [10, 20, 30]
     '''
-    currency =          # complete this line
+    currency = {'Ten': 10, 'Twenty': 20, 'Thirty': 30}          # complete this line
 
     return currency
 
@@ -15,14 +15,14 @@ def add_element():
     foods = {'fruit': 'apple', 'veggie': 'carrot', 'grain': 'barley'}
     # add a key value pair 'dairy': 'yogurt' to the following dictionary
 
-    # add code here
+    foods['dairy'] = 'yogurt'
 
     return foods
 
 def remove_element():
     foods = {'fruit': 'apple', 'veggie': 'carrot', 'grain': 'barley'}
     # remove 'veggie': 'carrot' from the dictionary
-
+    del foods['veggie']
 
     return foods
 
@@ -30,15 +30,14 @@ def merge_dict():
     # Merge these two dictionaries together so the contents are in numerical order:
     dict1 = {'Ten': 10, 'Twenty': 20, 'Thirty': 30}
     dict2 = {'Thirty': 30, 'Fourty': 40, 'Fifty': 50}
-    # add code here
-
-    return     # return new dictionary
+    dict3 = dict1.update({'Fourty': 40, 'Fifty': 50})
+    return dict1    # return new dictionary
 
 def access_key():
     # return the value of the key 'Twenty'
     currency = {'Ten': 10, 'Twenty': 20, 'Thirty': 30}
 
-    val =             # add code to assign the desired value to 'val'
+    val = currency.pop('Twenty')            # add code to assign the desired value to 'val'
     return val
 
 
